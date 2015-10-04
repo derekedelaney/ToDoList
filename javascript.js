83477ea02740599533c4b4e7532f02cff6e5384c
derek.e.delaney@gmail.com <Derek.e.delaney@gmail.com>
2015-10-03 18:19:22 -0700

    Added Edit/Delete button and checkboxes.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
     $("ol").keyup(function(){
-        $("li").keyup(addItem);
+        $("li").keyup(addAndRemoveItem);
     });
+
+    $("button").click(toggleButton);
+
+    $("#editButton").click(function() {
+        $("ol").replaceWith($('<ul>'+$("ol").html()+'</ul>'));
+        $('<input class="checkbox" type="checkbox">').prependTo('li');
+    });
+
+    $("#deleteButton").click(function() {
+        $("ul").replaceWith($('<ol>'+$("ul").html()+'</ul>'));
+        $(".checkbox").remove();
+    });
+
 });
 
-function addItem(event){
-    if(event.keyCode == 8 && $(this).children().val() === "" && $(this).next().is("li")){
+function addAndRemoveItem(event){
+    if(event.keyCode == 8 && $(this).children().val() === "" && $("li").size() !== 1){
         $(this).prev().children().focus();
         $(this).remove();
 
         return;
     }
-    if(!$(this).next().is("li") && event.keyCode !== 8)
-        $("ol").append('<li class="listItem"><input type="text"></li>');
+    if(!$(this).next().is("li") && event.keyCode !== 8 && event.keyCode === 13){
+        $("ol").append('<li class="listItem"><input class="textField" type="text"></li>');
+        $(this).next().children().focus();
+    }
+}
+
+function toggleButton(){
+    $("button").toggleClass("hidden");
 }
